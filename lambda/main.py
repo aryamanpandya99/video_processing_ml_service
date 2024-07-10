b5285958b7c5bc407f2d24e057005519735123ef
aryamanpandya99 <aryaman.pandya@tufts.edu>
2024-07-09 22:27:51 -0400

    s3 based lambda trigger successfully executed

---
+++ lambda/main.py
@@ -0,0 +1,25 @@
+import json
+
+def handler(event, context):
+    # Log the event to CloudWatch
+    print("Received event: " + json.dumps(event, indent=2))
+    
+    try:
+        for record in event['Records']:
+            s3 = record['s3']
+            bucket = s3['bucket']['name']
+            key = s3['object']['key']
+            
+            print(f"Bucket: {bucket}")
+            print(f"Key: {key}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps('An error occurred')
+        }
+        
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Success')
+    }
