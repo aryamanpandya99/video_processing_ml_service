d3c78ac4218541f65ed34600c72eee80623eaebd
aryamanpandya99 <aryaman.pandya@tufts.edu>
2024-07-10 23:53:14 -0400

    completed implementation

--- detection_service/object_detection.py
+++ detection_service/object_detection.py
@@ -1,6 +1,6 @@
 """
-Object detection application. 
-In: s3 image paths 
+Object detection application.
+In: s3 image paths
 Out: detection values written to MongoDB
 
 Author: Aryaman Pandya
@@ -11,38 +11,38 @@ import json
 
 import boto3
 import torch
-import torchvision.transforms.functional as F
 from PIL import Image
 from ultralytics import YOLO
 
 
-def frames_from_s3_paths(
-    s3_paths: list,
-) -> torch.Tensor:
+def frames_from_paths(
+    paths: list,
+    is_aws: bool
+) -> list:
     """
     Given paths to images in s3, returns a torch tensor of
     these image frames stacked.
 
     Args:
-        s3_paths (list[str])
+        paths (list[str])
+        is_aws (bool): whether paths are s3 paths or local
+
     Returns:
-        stacked_frames (torch.Tensor)
+        frames (list[PIL.Image])
     """
-    s3 = boto3.client("s3")
-    stacked_frames = None
+    aws_client = boto3.client("s3")
+    frames = []
+    for path in paths:
+        if is_aws:
+            response = aws_client.get_object(Bucket="video-aws-bucket", Key=path)
+            image_bytes = response["Body"].read()
+            image = Image.open(io.BytesIO(image_bytes))
+        else: 
+            image = Image.open(path)
+        
+        frames.append(image) # YOLO takes care of the tensore conversion
 
-    for path in s3_paths:
-        response = s3.get_object(Bucket="video-aws-bucket", Key=path)
-        image_bytes = response["Body"].read()
-        image = Image.open(io.BytesIO(image_bytes))
-        frame = F.pil_to_tensor(image)
-
-        if stacked_frames is None:
-            stacked_frames = frame.unsqueeze(0)  # Add batch dimension
-        else:
-            stacked_frames = torch.cat((stacked_frames, frame.unsqueeze(0)), dim=0)
-
-    return stacked_frames
+    return frames
 
 
 def object_detection(model, frames: torch.Tensor) -> list:
@@ -52,19 +52,19 @@ def object_detection(model, frames: torch.Tensor) -> list:
     Args:
         model: torch.nn.Module
         frames: torch.Tensor
-    
+
     Returns:
         results: list[json]
     """
     return [json.loads(pred.tojson()) for pred in model.predict(frames, stream=True)]
 
 
-def main(frame_paths: list):
+def main(frame_paths: list, is_aws: bool):
     """
     Runs the pipeline end to end: s3 paths -> write to db
     """
-    frames = frames_from_s3_paths(frame_paths)
-    model = YOLO("yolov9c")
+    frames = frames_from_paths(frame_paths, is_aws)
+    model = YOLO(model="models/yolov9c.pt")
     if torch.cuda.is_available():
         model = model.to("cuda")
 
@@ -75,6 +75,8 @@ def main(frame_paths: list):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-p','--paths', nargs='+', help='List of s3 paths', required=True)
+    parser.add_argument('--aws', action='store_true')
+    
     args = parser.parse_args()
 
-    main(args.paths)
+    main(args.paths, args.aws)
--- lambda/main.py
+++ lambda/main.py
@@ -13,14 +13,14 @@ def handler(event, context):
     print(f"Context: {context}")
     try:
         for record in event["Records"]:
-            s3 = record["s3"]
-            bucket = s3["bucket"]["name"]
-            key = s3["object"]["key"]
+            container = record["s3"]
+            bucket = container["bucket"]["name"]
+            key = container["object"]["key"]
 
             print(f"Bucket: {bucket}")
             print(f"Key: {key}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    except Exception as exc:
+        print(f"An error occurred: {exc}")
         return {"statusCode": 500, "body": json.dumps("An error occurred")}
 
     return {"statusCode": 200, "body": json.dumps("Success")}
